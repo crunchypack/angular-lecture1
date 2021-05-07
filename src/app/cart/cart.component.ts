2fdb34f10d64e8b984450c191fc2891787a3e3de
crunchypack <simon.wolf.lobo@gmail.com>
2021-05-07 16:28:18 -0400

    Checkout function

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
 import { CartService } from '../cart.service';
 
 @Component({
@@ -8,8 +10,25 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
   items = this.cartService.getItems(); // Gets array of items and stores it in items variable
+  checkoutForm = this.formBuilder.group({
+    // Using FormBuilder method group() to set a form model
+    // Fields
+    name: '',
+    address: ''
+  });
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
 
+  /**
+   *
+   */
+  onSubmit(): void {
+    this.items = this.cartService.cleanCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
   ngOnInit() {}
 }
